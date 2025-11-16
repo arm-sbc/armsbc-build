d01c50fd91dc7b225d9062b9e46358073233a1e3
ARM-SBC <support@arm-sbc.com>
2025-11-16 14:07:38 +0400

    Update board_config.sh

--- boards/sunxi/ARM-SBC-RP-A40i/board_config.sh
+++ boards/sunxi/ARM-SBC-RP-A40i/board_config.sh
@@ -5,4 +5,5 @@ CROSS_COMPILE="arm-linux-gnueabihf-"
 UBOOT_DEFCONFIG="armsbc-rp-a40i_defconfig"
 KERNEL_DEFCONFIG="armsbc-a40i_defconfig"
 DEVICE_TREE="sun8i-a40i-armsbc-rp.dts"
+DESKTOP_FLAVOR=lxqt
 
