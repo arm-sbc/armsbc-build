787e8f290f0124dbfeffab541113774746aaf0b4
ARM-SBC <support@arm-sbc.com>
2025-11-16 14:07:24 +0400

    Update board_config.sh

--- boards/sunxi/ARM-SBC-RP-T527/board_config.sh
+++ boards/sunxi/ARM-SBC-RP-T527/board_config.sh
@@ -5,4 +5,5 @@ CROSS_COMPILE="aarch64-linux-gnu-"
 UBOOT_DEFCONFIG="armsbc-rp-a527_defconfig" 
 KERNEL_DEFCONFIG="armsbc-a527_defconfig"
 DEVICE_TREE="sun55i-a527-armsbc-rp.dts" 
+DESKTOP_FLAVOR=lxqt
 
