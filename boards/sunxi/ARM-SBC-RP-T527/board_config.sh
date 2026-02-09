22c6275f71b2b9dd160c618e86cc6650278df549
ARM-SBC <support@arm-sbc.com>
2026-02-09 11:00:28 +0400

    Update board_config.sh a527

--- boards/sunxi/ARM-SBC-RP-T527/board_config.sh
+++ boards/sunxi/ARM-SBC-RP-T527/board_config.sh
@@ -1,9 +1,9 @@
-CHIP="t527"
-PROCESSOR_FAMILY="sun55i"
+CHIP="a527"
+PROCESSOR_FAMILY="sun55i_a523"
 ARCH="arm64"
 CROSS_COMPILE="aarch64-linux-gnu-"
-UBOOT_DEFCONFIG="armsbc-rp-a527_defconfig" 
+UBOOT_DEFCONFIG="armsbc-pro-a527_defconfig" 
 KERNEL_DEFCONFIG="armsbc-a527_defconfig"
-DEVICE_TREE="sun55i-a527-armsbc-rp.dts" 
+DEVICE_TREE="sun55i-a527-armsbc-pro.dts" 
 DESKTOP_FLAVOR=lxqt
 
